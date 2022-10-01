31ab8ab95483741b7d975dd9411c693e36428d62
Lucky2245 <95869708+Lucky2245@users.noreply.github.com>
2022-09-30 22:44:22 -0400

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,10 @@
+<?php 
+   setCookie("test_cookie", "test", time() + 3600, '/')
+?>
 <?php
    if(count($_COOKIE) > 0){
      echo "Cookies are Enabled!";
    }else{
+      echo "Enable Cookies!";
   }
 ?>
